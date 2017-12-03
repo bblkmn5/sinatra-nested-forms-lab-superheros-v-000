20c93ba78fc468a005daca6d8b679eb2a4b58188
Ben Blackman <bblkmn5@gmail.com>
2017-12-03 22:37:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,13 @@ class App < Sinatra::Base
     end
 
     post '/team' do
+      @team = Team.new(params[:team])
 
+      params[:team][:super_heroes].each do |details|
+        SuperHero.new(details)
+      end
+      @super_heroes = SuperHero.all 
+
+      erb :team
     end
 end
